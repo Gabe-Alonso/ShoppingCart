3b37bfa107089e49b543a2f6bf5bbfb07f291b3d
TJ5__ <tejesh.anand@hotmail.com>
2020-12-02 09:34:32 -0800

    git pull

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,18 +1,32 @@
 import java.util.*;
+//TJ
+//12/2/20
+//Shopping Cart Lab
 public class ShoppingCart {
     private ArrayList<ItemOrder> cart = new ArrayList<ItemOrder>();
     private boolean discount;
+    //Default constructor
     public ShoppingCart() {
         return;
     }
+    //Adds an order to the cart or overrides an existing order for the same product
     public void add(ItemOrder order) {
-        cart.add(order);
-    }
+        for (int i = 0; i < cart.size(); i++) {
+            if (cart.get(i).getItem().toString().equals(order.getItem().toString())) {
+                cart.set(i, order);
+            }
+            else {
+                cart.add(order);
+            }
+        }
+        
 
+    }
+    //Sets whether there is a discount
     public void setDiscount(boolean d) {
         discount = d;
     }
-
+    //Returns the total cost of the cart
     public double getTotal() {
         double price = 0;
         for (int i = 0; i < cart.size(); i++) {
