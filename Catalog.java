08e8dd3504ad09b802f281608a643eecc715d114
Gabe-Alonso <67928782+Gabe-Alonso@users.noreply.github.com>
2020-12-02 09:05:08 -0800

    Update Catalog.java

--- Catalog.java
+++ Catalog.java
@@ -1,3 +1,24 @@
 public class Catalog {
-    
+    private String name;
+    private ArrayList<Item> catalog;
+
+    public Catalog(String nameP){
+        name = nameP;
+    }
+
+    public void add(Item it){
+        catalog.add(it);
+    }
+
+    public int size(){
+        return catalog.size();
+    }
+
+    public Item get(int i){
+        return catalog.get(i);
+    }
+
+    public String getName(){
+        return name;
+    }
 }
