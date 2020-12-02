7efa9214aea9e32a55a87f4c820b5c5d5ce4af78
Yash Mehta <imyash@live.com>
2020-12-02 09:15:56 -0800

    Update ItemOrder

--- ItemOrder.java
+++ ItemOrder.java
@@ -1,3 +1,20 @@
 public class ItemOrder {
-    
+    private Item product; 
+    private int numberOf;
+
+    public ItemOrder (Item item, int quantity)
+    {
+        this.product = item;
+        this.numberOf = quantity;
+    }
+
+    public double getPrice(String item)
+    {
+        return product.priceFor(numberOf);
+    }
+
+    public Item getItem()
+    {
+        return product;
+    }
 }
