074cf816b1942fb43735a42c2734a3f595632efb
Yash Mehta <imyash@live.com>
2020-12-02 09:17:42 -0800

    added comments

--- ItemOrder.java
+++ ItemOrder.java
@@ -2,17 +2,20 @@ public class ItemOrder {
     private Item product; 
     private int numberOf;
 
+    //constructor given item and quantity
     public ItemOrder (Item item, int quantity)
     {
         this.product = item;
         this.numberOf = quantity;
     }
 
+    //return cost of said item 
     public double getPrice(String item)
     {
         return product.priceFor(numberOf);
     }
 
+    //return reference to item
     public Item getItem()
     {
         return product;
