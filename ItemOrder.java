9c5a1eb96b867b496ef100a4ff089d0455550ead
Yash Mehta <imyash@live.com>
2020-12-02 10:01:09 -0800

    class, method, and general comments on ItemOrder

--- ItemOrder.java
+++ ItemOrder.java
@@ -1,3 +1,10 @@
+//Written by Yash Mehta
+//December 2, 2020
+//ItemOrder Class
+//This class stores information about certain item as well as the quantity ordered.
+//Its methods aid other processes and return the price as well as a reference to the item ordered. 
+
+
 public class ItemOrder {
     private Item product; 
     private int numberOf;
@@ -9,13 +16,13 @@ public class ItemOrder {
         this.numberOf = quantity;
     }
 
-    //return cost of said item 
+    //return cost of said item, looks to Item class for price
     public double getPrice()
     {
         return product.priceFor(numberOf);
     }
 
-    //return reference to item
+    //return reference to item ordered
     public Item getItem()
     {
         return product;
